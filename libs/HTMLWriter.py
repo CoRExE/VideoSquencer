d604abf120ad4b5402bb983dade2fba803f5d925
Jules Langagne <langagnejules@gmail.com>
2024-02-14 16:57:14 +0100

    HTML Redact part 2

--- libs/HTMLWriter.py
+++ libs/HTMLWriter.py
@@ -6,26 +6,64 @@ class HTMLReportWriter:
     def __init__(self, folder_path):
         self.folder_path = folder_path
 
-    def write_report(self, report_name, folder_path):
+    def write_report(self, report_name):
         if not os.path.exists(self.folder_path + '/report'):
             os.mkdir(self.folder_path + '/report')
 
         # Read the index.html template
-        with open('../template/index.html', 'r') as file:
+        with open('template/index.html', 'r') as file:
             index_template_content = file.read()
 
         # Create a Jinja2 template object for index.html
         index_template = Template(index_template_content)
 
         # Render the index.html template with the report data
-        index_result = index_template.render(report_name=report_name, content=self.generate_cards(folder_path))
+        index_result = index_template.render(report_name=report_name, content=self.generate_cards(self.folder_path))
 
         # Write the rendered index.html to the report folder
         with open(self.folder_path + '/report/index.html', 'w') as file:
             file.write(index_result)
 
     def generate_cards(self, folder_path):
-        pass
+        content = ""
 
-    def generate_card(self, folder_name, folder_path):
-        pass
+        for nom_fichier in os.listdir(folder_path):
+            chemin_fichier = folder_path + '/' + nom_fichier
+            if os.path.isdir(chemin_fichier):
+                content += self.generate_card(chemin_fichier)
+                self.generate_list(chemin_fichier)
+
+        return content
+
+    @staticmethod
+    def generate_card(folder):
+        # Read the frame_list.html template
+        with open('template/card.html', 'r') as file:
+            card_template_content = file.read()
+
+        # Create a Jinja2 template object for frame_list.html
+        card_template = Template(card_template_content)
+
+        # Render the frame_list.html template with the report data
+        return card_template.render(folder=folder, number_files=len(os.listdir(folder)),
+                                    folder_dest="frame_list_" + folder.split('/')[-1] + ".html")
+
+    def generate_list(self, folder):
+        # Read the frame_list.html template
+        with open('template/frame_list.html', 'r') as file:
+            frame_list_template_content = file.read()
+
+        # Create a Jinja2 template object for frame_list.html
+        frame_list_template = Template(frame_list_template_content)
+
+        # Render the frame_list.html template with the report data
+        frame_list_result = frame_list_template.render(folder_path=folder, title=folder)
+
+        # Write the rendered frame_list.html to the report folder
+        with open(self.folder_path + '/report/frame_list_' + folder.split('/')[-1] + '.html', 'w') as file:
+            file.write(frame_list_result)
+
+
+# Testing #
+essai = HTMLReportWriter(r"C:/Users/client/Documents/Python Scripts/VideoSquencer/Result")
+essai.write_report("test")
