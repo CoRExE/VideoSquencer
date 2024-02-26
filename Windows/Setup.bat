bc5c4b24b986b9faa0031f022493aa2131b00415
Jules Langagne <langagnejules@gmail.com>
2024-02-26 14:45:47 +0100

    Setup and Running Correction

--- Windows/Setup.bat
+++ Windows/Setup.bat
@@ -1,3 +1,11 @@
+echo "If the next command line crash, check your Python Installation"
+
+timeout 3
+
+python -V
+
+timeout 5
+
 pip install -U opencv-python
 pip install -U Jinja2
 pip install -U numpy
@@ -6,4 +14,6 @@ echo "Requirement Installed"
 
 timeout 3
 
-move Run.bat ../
+attrib -h Run.bat
+
+copy Run.bat ../
